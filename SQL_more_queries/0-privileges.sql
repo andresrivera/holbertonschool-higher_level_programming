0e7beff577217e75f2d40f9993475cf4b7516cdd
andresrivera <andresrivera.web@gmail.com>
2023-07-27 09:14:41 -0700

    add changes in task n.0 file

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,5 +3,5 @@
 -- SELECT CONCAT('SHOW GRANTS FOR ''',user_0d_1,'''@''',localhost,''';') FROM mysql.user;
 -- SELECT CONCAT('SHOW GRANTS FOR ''',user_0d_2,'''@''',localhost,''';') FROM mysql.user;
 
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_02_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_02_2'@'localhost';
