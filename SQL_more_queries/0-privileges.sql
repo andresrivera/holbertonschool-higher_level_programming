736b5b4c4e91c6a9489aae258bb33004d3d3d527
andresrivera <andresrivera.web@gmail.com>
2023-07-27 09:16:57 -0700

    add changes in task n.0 file

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
--- SELECT CONCAT('SHOW GRANTS FOR ''',user_0d_1,'''@''',localhost,''';') FROM mysql.user;
--- SELECT CONCAT('SHOW GRANTS FOR ''',user_0d_2,'''@''',localhost,''';') FROM mysql.user;
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_02_2'@'localhost';
